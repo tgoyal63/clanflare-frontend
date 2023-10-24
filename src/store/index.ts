acf36624a47242932302a60ba481538cf20b83bf
saketverma-03 <saketv831@gmail.com>
2023-10-25 00:19:37 +0530

    ADDED: global variable

---
+++ src/store/index.ts
@@ -0,0 +1,16 @@
+import { atom } from "jotai";
+import * as z from "zod";
+
+const otpDataSechema = z.object({
+  phone: z.coerce.number().int().min(1000000000).max(9999999999),
+  otpHash: z.string(),
+  expiresAt: z.coerce.number().int(),
+});
+
+export type OtpDataType = z.infer<typeof otpDataSechema>;
+
+export const otpDataAtom = atom({
+  phone: 0,
+  otpHash: "",
+  expiresAt: 0,
+});
