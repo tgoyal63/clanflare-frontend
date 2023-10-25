db68723171536bca29c0601430e0ae24b91cc14f
saketverma-03 <saketv831@gmail.com>
2023-10-25 18:33:54 +0530

    ADDED: shad cn ui component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as NavbarAvatar } from "@/components/Navigation/AvatarNav";
 export { default as AddNewServerCard } from "@/components/addNewServer/Card";
 export { default as CardDashboard } from "@/components/dashboard/CardComponent";
+export { default as TestiMonialCards } from "@/components/landingpage/testimonials";
 export { default as QueryProvider } from "@/components/wrappers/QueryProvider";
