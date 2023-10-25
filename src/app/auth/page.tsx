526fe31abb37862140a7f31bb7fcb880505ae428
saketverma-03 <saketv831@gmail.com>
2023-10-25 18:33:05 +0530

    ADDED: Auth page know redirects to login api

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,6 @@
-export default function AuthPage() {
-  return (
-    <>
-      <div className="text-3xl">Please login ,you are unauthorised</div>
-    </>
-  );
+import { redirect } from "next/navigation";
+
+export default async function Profile() {
+  console.log(process.env.BASE_AUTH_URL);
+  redirect(process.env.BASE_API_URL + "/login");
 }
