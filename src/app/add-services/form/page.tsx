e2f096800eb96ab560fe0752abb4b8c9e98588ec
saketverma-03 <saketv831@gmail.com>
2023-10-29 01:08:30 +0530

    ADDED: global state for add new server page

--- src/app/add-services/form/page.tsx
+++ src/app/add-services/form/page.tsx
@@ -1,6 +1,22 @@
-import { redirect } from "next/navigation";
+"use client";
+import { addNewServerAtom } from "@/store/addNewServer";
+import { useAtom } from "jotai";
+import { redirect, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function Profile() {
-  console.log(process.env.BASE_AUTH_URL);
-  redirect("./form/add-bot-to-server");
+export default function Profile() {
+  const [, setServiceData] = useAtom(addNewServerAtom);
+  const params = useSearchParams();
+  const route = useRouter();
+  const id = params.get("id");
+
+  useEffect(() => {
+    if (!id) return route.push("./");
+    setServiceData((state) => {
+      state.server.id = id;
+
+      return state;
+    });
+    redirect("./form/add-bot-to-server");
+  }, [id]);
 }
--- src/utils/googlesheet.ts
+++ src/utils/googlesheet.ts
@@ -1,26 +1,8 @@
-// url
-// const url =
-//   "https://docs.google.com/spreadsheets/d/1sLT8Rn1_MvmOt_IWGC3Uda5y4elfEp3wtdgJbn0Vbb8/edit?usp=sharing";
-
-// const match = url.match(/\/d\/(.*?)\/edit/);
-
-// if (match) {
-//   const extractedString = match[1];
-//   console.log(extractedString); // This will print '1sLT8Rn1_MvmOt_IWGC3Uda5y4elfEp3wtdgJbn0Vbb8'
-// } else {
-//   console.log("Match not found");
-// }
-
-/** extracts id from googleSheet url
- * ex-input: "https://docs.google.com/spreadsheets/d/1sLT8Rn1_MvmOt_IWGC3Uda5y4elfEp3wtdgJbn0Vbb8/edit?usp=sharing";
- * output: 1sLT8Rn1_MvmOt_IWGC3Uda5y4elfEp3wtdgJbn0Vbb8
- */
 export function extractIdFromSheetUrl(url: string) {
   const match = url.match(/\/d\/(.*?)\/edit/);
 
   if (match) {
     const extractedString = match[1];
-    // console.log(extractedString); // This will print '1sLT8Rn1_MvmOt_IWGC3Uda5y4elfEp3wtdgJbn0Vbb8'
     return extractedString;
   } else {
     console.log("Match not found");
