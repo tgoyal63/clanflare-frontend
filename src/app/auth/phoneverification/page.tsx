c9ff5b35fbe10e3200b09d3026582ba7c924e2e2
saketverma-03 <saketv831@gmail.com>
2023-10-28 09:04:28 +0530

    ADDED: skip funtonality to form and removed token storage from here

--- src/app/auth/phoneverification/page.tsx
+++ src/app/auth/phoneverification/page.tsx
@@ -3,6 +3,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import indiaFlag from "@/assits/in.svg";
+import { ThemeToggle } from "@/components";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import {
@@ -15,7 +17,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-
 import {
   Select,
   SelectContent,
@@ -24,20 +25,17 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useToast } from "@/components/ui/use-toast";
+import { Loader2, X } from "lucide-react";
 
-import { useMutation } from "@tanstack/react-query";
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { useLocalStorage } from "usehooks-ts";
-
-import { ThemeToggle } from "@/components";
 import { OtpDataType, otpDataAtom } from "@/store";
 import { axios } from "@/utils/server";
-import { useAtom } from "jotai";
-import { Loader2 } from "lucide-react";
-import Image from "next/image";
 
-import indiaFlag from "@/assits/in.svg";
+import { useMutation } from "@tanstack/react-query";
+import { useAtom } from "jotai";
+import { useRouter } from "next/navigation";
+
+import Image from "next/image";
+import Link from "next/link";
 
 const PhoneNumberFormSchema = z.object({
   phoneNumber: z.coerce
@@ -56,10 +54,8 @@ export default function PhoneVerification() {
   // -
 
   const router = useRouter();
-  const params = useSearchParams();
-  const token = params.get("token");
   const { toast } = useToast();
-  const [auth, setLocalVal] = useLocalStorage("auth", "");
+  // const [auth, setAuth] = useLocalStorage("auth", "");
   const [, setOtpData] = useAtom(otpDataAtom);
 
   const form = useForm<z.infer<typeof PhoneNumberFormSchema>>({
@@ -70,14 +66,8 @@ export default function PhoneVerification() {
     },
   });
 
-  // if no token in param
-  useEffect(() => {
-    if (token) setLocalVal(token);
-    else if (auth) return;
-    else router.push("/auth");
-  });
-
-  const { mutate: createOtp, isPending: isCreating } = useMutation({
+  // hadnling form submission
+  const { mutate: createOtp, isPending: isLoading } = useMutation({
     mutationKey: ["otp"],
     mutationFn: async (phone: number) => {
       const res = await axios.post(`/send-otp`, {
@@ -110,15 +100,24 @@ export default function PhoneVerification() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
-      <div className="z-10 flex h-screen w-full max-w-5xl  flex-col items-center justify-center text-sm">
-        <div className="mb-2 flex w-full max-w-md justify-end">
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="z-10 flex h-screen w-full max-w-5xl flex-col items-center justify-center p-2 text-sm">
+        <div className="mb-2 flex w-full max-w-md justify-end gap-2">
           <ThemeToggle />
         </div>
-        <Card className="mb-48 w-screen max-w-md border-0 px-2 py-4 sm:mb-0  sm:w-full sm:border sm:p-4 sm:shadow-md md:p-6">
-          <h1 className="mb-4 text-2xl">
-            Phone Verification <br />
-            <span className="text-lg">Generate an OTP </span>
+        <Card className="mx-5 mb-48 w-full max-w-md border-0 px-2 py-4 sm:mb-0  sm:w-full sm:border sm:p-4 sm:shadow-md md:p-6">
+          <h1 className="mb-4 flex flex-col text-2xl sm:flex-row ">
+            <div className="w-full">
+              Phone Verification <br />
+              <span className="text-lg opacity-75">Generate an OTP </span>
+            </div>
+            <div className="mt-2 sm:m-0">
+              <Link href="/dashboard">
+                <Button variant={"ghost"}>
+                  <X />
+                </Button>
+              </Link>
+            </div>
           </h1>
 
           <Form {...form}>
@@ -178,14 +177,20 @@ export default function PhoneVerification() {
                   )}
                 />
               </div>
-
-              <Button type="submit" className="w-full" disabled={isCreating}>
-                {isCreating ? (
-                  <Loader2 className="animate-spin" />
-                ) : (
-                  "Generate OTP"
-                )}
-              </Button>
+              <div className="grid grid-cols-2 gap-2">
+                <Button type="submit" className="w-full" disabled={isLoading}>
+                  {isLoading ? (
+                    <Loader2 className="animate-spin" />
+                  ) : (
+                    "Generate OTP"
+                  )}
+                </Button>
+                <Link href="/dashboard">
+                  <Button className="w-full" variant={"secondary"}>
+                    Skip Verification
+                  </Button>
+                </Link>
+              </div>
             </form>
           </Form>
         </Card>
