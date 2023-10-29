2ee40326fa5de22c0134d706b3a90f8e7924b97c
saketverma-03 <saketv831@gmail.com>
2023-10-29 14:27:33 +0530

    UPDATE: if user already logedin than redirect to dashboard

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,17 @@
+"use client";
+import { useAuthenticator } from "@/hooks/useAuthenticator";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
-export default async function Profile() {
+export default function Profile() {
+  const { token } = useAuthenticator();
+
+  useEffect(() => {
+    if (!token) {
+      redirect(process.env.NEXT_PUBLIC_BASE_API_URL + "/login");
+    } else {
+      redirect("/dashboard");
+    }
+  }, [token]);
   // console.log(process.env.BASE_AUTH_URL);
-  redirect(process.env.NEXT_PUBLIC_BASE_API_URL + "/login");
 }
