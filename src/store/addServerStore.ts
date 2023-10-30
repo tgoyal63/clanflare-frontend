18dcf6ec651c52fa778f6bd5a03e6730ebe0cf4b
saketverma-03 <saketv831@gmail.com>
2023-10-30 14:28:10 +0530

    UDPATED: store

--- src/store/addServerStore.ts
+++ src/store/addServerStore.ts
@@ -12,35 +12,83 @@ type AddNewServerData = {
     isVerified: boolean;
   };
   googleSheet: {
-    id: string;
+    url: string;
+    allSheets: sheetData[];
+    selectedSheet: sheetData;
   };
+};
+
+export type sheetData = {
+  index: number;
+  title: string;
+  sheetId: number;
 };
 
 type Actions = {
   updateServer: (id: string, isVerified: boolean) => void;
-  updateGoogleSeheet: (id: string, isVerified: boolean) => void;
+  /** replace all sheet array  */
+  updateGoogleSheetData: (data: sheetData[]) => void;
+  /** update url*/
+  updateGoogleSheetUrl: (url: string) => void;
+  updateSelectedSheet: (data: sheetData) => void;
+};
+
+const defaultData = {
+  googleSheet: {
+    url: "",
+    allSheets: [],
+    selectedSheet: {
+      index: 0,
+      sheetId: 0,
+      title: "",
+    },
+  },
+
+  server: {
+    id: "",
+    isVerified: false,
+  },
 };
 
 export const useNewServerStore = create<AddNewServerData & Actions>()(
   persist(
-    (set) => ({
-      googleSheet: {
-        id: "",
-      },
-      server: {
-        id: "",
-        isVerified: false,
-      },
-      updateGoogleSeheet: (id) => {
-        const temp = { id: id };
-        return set((state) => ({ ...state, temp }));
-      },
+    (set, get) => ({
+      googleSheet: defaultData.googleSheet,
+      server: defaultData.server,
+
       updateServer: (id, isVerified) => {
         const newServer = {
           id,
           isVerified,
         };
         return set((state) => ({ ...state, server: newServer }));
+      },
+      updateGoogleSheetData: (data) => {
+        return set((s) => ({
+          ...s,
+          googleSheet: {
+            ...s.googleSheet,
+            allSheets: data,
+          },
+        }));
+      },
+      updateSelectedSheet: (data) => {
+        return set((state) => ({
+          ...state,
+          googleSheet: {
+            ...state.googleSheet,
+            selectedSheet: data,
+          },
+        }));
+      },
+      updateGoogleSheetUrl: (url) => {
+        return set((state) => ({
+          ...state,
+          googleSheet: {
+            ...state.googleSheet,
+            url: url,
+          },
+        }));
       },
     }),
     {
