8f31f9108feb28fb5f1388b5e6d67bf710592138
Saket verma <saketverma831@gmail.com>
2023-11-04 03:58:12 +0530

    UPDATED

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,16 +1,35 @@
 "use client";
 import { useUserStore } from "@/store";
+import axios from "axios";
 import { redirect } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Profile() {
   const token = useUserStore((state) => state.token);
 
+  const getUrl = async () => {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_BASE_API_URL}/oauth-link`,
+      {
+        headers: {
+          "ngrok-skip-browser-warning": true,
+        },
+      },
+    );
+    const newURL = res.data?.data.oauthLink as string;
+    redirect(newURL);
+  };
+
   useEffect(() => {
     if (!token) {
-      redirect(process.env.NEXT_PUBLIC_BASE_API_URL + "/login");
+      if (process.env.NEXT_PUBLIC_AUTH_URL) {
+        redirect(process.env.NEXT_PUBLIC_AUTH_URL);
+      } else {
+        getUrl();
+      }
     } else {
       redirect("/dashboard");
     }
   }, [token]);
+  return <>redirecting</>;
 }
